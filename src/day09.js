bf6ec71e705b526d8389e92eac7b920d06e4019e
Jonas Krogh <jonas.krogh@statnett.no>
2024-12-09 12:25:29 +0100

    Alternative 9.2 implementation

--- src/day09.js
+++ src/day09.js
@@ -3,19 +3,19 @@ import { readFileSync } from "node:fs";
 const input = readFileSync(process.stdin.fd, "utf-8").trim();
 
 const blocks = [];
+const part1Blocks = [];
 
 input.split("").forEach((e, i) => {
   const num = parseInt(e);
+  blocks.push({ id: i, count: num });
   for (let j = 0; j < num; ++j) {
     if (i & 1) {
-      blocks.push(undefined);
+      part1Blocks.push(undefined);
     } else {
-      blocks.push(i / 2);
+      part1Blocks.push(i / 2);
     }
   }
 });
-
-const part1Blocks = [...blocks];
 
 for (let i = part1Blocks.length - 1; i > 0; --i) {
   const freeIndex = part1Blocks.indexOf(undefined);
@@ -31,27 +31,30 @@ const part1 = part1Blocks.reduce((p, c, i) => {
 }, 0);
 
 for (let i = (input.length - 1) / 2; i > 0; --i) {
-  const count = parseInt(input[i * 2]);
-  const firstIndex = blocks.indexOf(i);
+  const blocksIndex = blocks.findIndex((e) => e.id === i * 2);
+  const count = blocks.at(blocksIndex).count;
 
-  const freeIndex = blocks.findIndex((e, j) => {
-    let match = true;
-    for (let k = 0; k < count; ++k) {
-      if (blocks.at(j + k) != undefined) {
-        match = false;
-        break;
-      }
+  const freeIndex = blocks.findIndex((e) => e.id & 1 && e.count >= count);
+  if (freeIndex > -1 && freeIndex < blocksIndex) {
+    const block = blocks.splice(blocksIndex, 1, { id: 0, count })[0];
+    if (blocks[freeIndex].count > count) {
+      blocks[freeIndex].count -= count;
+      blocks.splice(freeIndex, 0, block);
+    } else {
+      blocks.splice(freeIndex, 1, block);
     }
-    return match;
-  });
-  if (freeIndex > -1 && freeIndex < firstIndex) {
-    blocks.splice(freeIndex, count, ...new Array(count).fill(i));
-    blocks.splice(firstIndex, count, ...new Array(count).fill(undefined));
   }
 }
-const part2 = blocks.reduce((p, c, i) => {
-  if (c === undefined) return p;
-  return p + c * i;
+
+let idx = 0;
+const part2 = blocks.reduce((p, c) => {
+  if (c.id & 1) {
+    idx += c.count;
+    return p;
+  }
+  let ret = p + (c.id * c.count * (c.count + 2 * idx - 1)) / 4;
+  idx += c.count;
+  return ret;
 }, 0);
 
 console.log(part1);
