6b347f4290ad4e27614e20a55221e04c91974d25
Jonas Krogh <jonas.krogh@statnett.no>
2024-12-11 08:29:01 +0100

    Switch string to number

--- src/day11.js
+++ src/day11.js
@@ -5,30 +5,39 @@ const input = readFileSync(process.stdin.fd, "utf-8").trim();
 let stones = new Map();
 
 input.split(" ").forEach((e) => {
-  const count = stones.get(e) ?? 0;
-  stones.set(e, count + 1);
+  const num = parseInt(e);
+  const count = stones.get(num) ?? 0;
+  stones.set(num, count + 1);
 });
 
-for (let i = 0; i < 75; ++i) {
+for (let i = 1; i <= 75; ++i) {
   let newStones = new Map();
   for (const [stone, count] of stones) {
-    if (stone === "0") {
-      const cc = newStones.get("1") ?? 0;
-      newStones.set("1", count + cc);
-    } else if (stone.length % 2 === 0) {
-      const str1 = stone.slice(0, stone.length / 2);
-      const str2 = "" + parseInt(stone.slice(stone.length / 2));
-      const c1 = newStones.get(str1) ?? 0;
-      newStones.set(str1, c1 + count);
-      const c2 = newStones.get(str2) ?? 0;
-      newStones.set(str2, c2 + count);
+    const log = Math.floor(Math.log10(stone));
+    if (stone === 0) {
+      const cc = newStones.get(1) ?? 0;
+      newStones.set(1, count + cc);
+    } else if (log & 1) {
+      const pow = Math.pow(10, (log + 1) / 2);
+      const first = Math.floor(stone / pow);
+      const second = stone % pow;
+
+      const c1 = newStones.get(first) ?? 0;
+      newStones.set(first, c1 + count);
+
+      const c2 = newStones.get(second) ?? 0;
+      newStones.set(second, c2 + count);
     } else {
-      const str = "" + parseInt(stone) * 2024;
-      const cc = newStones.get(str) ?? 0;
-      newStones.set(str, count + cc);
+      const num = stone * 2024;
+
+      const cc = newStones.get(num) ?? 0;
+      newStones.set(num, count + cc);
     }
   }
   stones = newStones;
+  if (i === 25) {
+    console.log(stones.values().reduce((p, c) => p + c, 0));
+  }
 }
 
-console.log(stones.entries().reduce((p, c) => p + parseInt(c[1]), 0));
+console.log(stones.values().reduce((p, c) => p + c, 0));
