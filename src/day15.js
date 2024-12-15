6554bb29e2a43d1fceff1ef2bc9569dc4d7ae265
Jonas Krogh <xyzkrgu@gmail.com>
2024-12-15 09:53:26 +0100

    Clean up day 15

--- src/day15.js
+++ src/day15.js
@@ -32,11 +32,13 @@ const dirs = new Map([
 
 for (const move of moves) {
   const dir = dirs.get(move);
-  if (makeMove(robotX1, robotY1, dir, map1)) {
+  if (canMove(robotX1, robotY1, dir, map1)) {
+    makeMove(robotX1, robotY1, dir, map1);
     robotX1 = robotX1 + dir[0];
     robotY1 = robotY1 + dir[1];
   }
-  if (makeMove(robotX, robotY, dir, map)) {
+  if (canMove(robotX, robotY, dir, map)) {
+    makeMove(robotX, robotY, dir, map);
     robotX = robotX + dir[0];
     robotY = robotY + dir[1];
   }
@@ -68,51 +70,24 @@ function makeMove(x, y, dir, map) {
   const newX = x + dir[0];
   const newY = y + dir[1];
 
-  if (map[newY][newX] == "#") {
-    return false;
-  }
-  if (map[newY][newX] == ".") {
-    map[newY][newX] = map[y][x];
-    map[y][x] = ".";
-    return true;
-  }
   if (map[newY][newX] == "O") {
-    if (makeMove(newX, newY, dir, map)) {
-      map[newY][newX] = map[y][x];
-      map[y][x] = ".";
-      return true;
-    }
-  }
-  if (dir[1] != 0) {
+    makeMove(newX, newY, dir, map);
+  } else if (dir[1] != 0) {
     if (map[newY][newX] == "[") {
-      if (canMove(newX, newY, dir) && canMove(newX + 1, newY, dir)) {
-        makeMove(newX, newY, dir, map);
-        makeMove(newX + 1, newY, dir, map);
-        map[newY][newX] = map[y][x];
-        map[y][x] = ".";
-        return true;
-      }
+      makeMove(newX, newY, dir, map);
+      makeMove(newX + 1, newY, dir, map);
+    } else if (map[newY][newX] == "]") {
+      makeMove(newX, newY, dir, map);
+      makeMove(newX - 1, newY, dir, map);
     }
-    if (map[newY][newX] == "]") {
-      if (canMove(newX, newY, dir) && canMove(newX - 1, newY, dir)) {
-        makeMove(newX, newY, dir, map);
-        makeMove(newX - 1, newY, dir, map);
-        map[newY][newX] = map[y][x];
-        map[y][x] = ".";
-        return true;
-      }
-    }
-  } else {
-    if (map[newY][newX] == "]" || map[newY][newX] == "[") {
-      if (makeMove(newX, newY, dir, map)) {
-        map[newY][newX] = map[y][x];
-        map[y][x] = ".";
-        return true;
-      }
-    }
+  } else if (map[newY][newX] == "]" || map[newY][newX] == "[") {
+    makeMove(newX, newY, dir, map);
   }
+  map[newY][newX] = map[y][x];
+  map[y][x] = ".";
 }
-function canMove(x, y, dir) {
+
+function canMove(x, y, dir, map) {
   const newX = x + dir[0];
   const newY = y + dir[1];
 
@@ -122,22 +97,19 @@ function canMove(x, y, dir) {
   if (map[newY][newX] == ".") {
     return true;
   }
+  if (map[newY][newX] == "O") {
+    return canMove(newX, newY, dir, map);
+  }
   if (dir[1] != 0) {
     if (map[newY][newX] == "[") {
-      if (canMove(newX, newY, dir) && canMove(newX + 1, newY, dir)) {
-        return true;
-      }
+      return canMove(newX, newY, dir, map) && canMove(newX + 1, newY, dir, map);
     }
     if (map[newY][newX] == "]") {
-      if (canMove(newX, newY, dir) && canMove(newX - 1, newY, dir)) {
-        return true;
-      }
+      return canMove(newX, newY, dir, map) && canMove(newX - 1, newY, dir, map);
     }
   } else {
     if (map[newY][newX] == "]" || map[newY][newX] == "[") {
-      if (canMove(newX, newY, dir)) {
-        return true;
-      }
+      return canMove(newX, newY, dir, map);
     }
   }
 }
