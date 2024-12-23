566dc81efcfb4e141634dab280a712c1f876eab6
Jonas Krogh <xyzkrgu@gmail.com>
2024-12-23 09:23:49 +0100

    Improve day 22

--- src/day22.js
+++ src/day22.js
@@ -2,7 +2,6 @@
 import { readFileSync } from "node:fs";
 
 const input = readFileSync(process.stdin.fd, "utf-8").trim();
-const lines = input.split("\n").map((e) => BigInt(e));
 
 const numberOfSecrets = 2000;
 
@@ -13,20 +12,24 @@ function* getSecret(n) {
   }
 }
 
+// 2**24 - 1
+const bm = 16777215;
+
 function transform(line) {
-  let a = ((line * 64n) ^ line) % 16777216n;
-  let b = (a / 32n) ^ a;
-  let c = ((b * 2048n) ^ b) % 16777216n;
+  let a = ((line * 64) ^ line) & bm;
+  let b = (a / 32) ^ a;
+  let c = ((b * 2048) ^ b) & bm;
   return c;
 }
 
 function toKey(a, b, c, d) {
-  return ((((((a + 16n) << 5n) + b + 16n) << 5n) + c + 16n) << 5n) + d + 16n;
+  return ((((((a + 16) << 5) + b + 16) << 5) + c + 16) << 5) + d + 16;
 }
 
-let part1 = 0n;
+let part1 = 0;
 const globalScore = new Map();
-for (const line of lines) {
+for (const line of input.split("\n")) {
+  const num = +line;
   const priceChanges = new Set();
 
   let a;
@@ -34,8 +37,8 @@ for (const line of lines) {
   let c;
   let d;
 
-  let prevPrice = line % 10n;
-  const secrets = getSecret(line);
+  let prevPrice = num % 10;
+  const secrets = getSecret(num);
 
   for (let i = 1; i <= numberOfSecrets; ++i) {
     a = b;
@@ -43,7 +46,7 @@ for (const line of lines) {
     c = d;
     const secret = secrets.next().value;
     if (i == numberOfSecrets) part1 += secret;
-    const price = secret % 10n;
+    const price = secret % 10;
     d = price - prevPrice;
     prevPrice = price;
     if (a == undefined) continue;
@@ -51,16 +54,16 @@ for (const line of lines) {
 
     if (!priceChanges.has(diffKey)) {
       priceChanges.add(diffKey);
-      const currentGlobal = globalScore.get(diffKey) ?? 0n;
+      const currentGlobal = globalScore.get(diffKey) ?? 0;
       globalScore.set(diffKey, currentGlobal + price);
     }
   }
 }
 
-let part2 = 0n;
+let part2 = 0;
 for (const score of globalScore.values()) {
   if (score > part2) part2 = score;
 }
 
-console.log(part1.toString());
-console.log(part2.toString());
+console.log(part1);
+console.log(part2);
